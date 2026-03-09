3d85b37eab02fa9b6072b9e73fd3270167195c43
ChethanRaj13 <chethanrajjee@gmail.com>
2026-03-09 05:58:14 +0530

    add few more program

--- practice1.py
+++ practice1.py
@@ -6,3 +6,33 @@ b = int(input("Enter the second number: "))
 sum = a + b
 
 print("sum: ", sum)
+
+# ------------------------------------------------
+
+# WAP to input side of a square and print it's area
+
+len = int(input("Enter the side of the square: "))
+
+sq = len * len
+
+print("area of the square of length", len, "is", sq ,"sqmeter")
+
+#--------------------------------------------------
+
+# WAP to input 2 floating point numbers and get there average
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+avg = ((a + b) / 2)
+
+print("Average:", avg)
+
+#---------------------------------------------------
+
+# WAP to input 2 numbers and print if a >= b
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print(a >= b)
