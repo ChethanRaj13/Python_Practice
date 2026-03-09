fba94e3a7b4b5a9f4fc4d045d00b70880183ce68
ChethanRaj13 <chethanrajjee@gmail.com>
2026-03-09 10:08:13 +0530

    list & tuples

---
+++ practice3.py
@@ -0,0 +1,36 @@
+# WAP to ask user enter 3 fav movie and store them in list
+
+# movie1 = input("Enter 1st fav movie: ")
+# movie2 = input("Enter 2nd fav movie: ")
+# movie3 = input("Enter 3rd fav movie: ")
+
+# list = [movie1, movie2, movie3]
+
+# print(list)
+
+#-----------------------------------------------------
+
+# WAP to check if the list is a pallindrome
+
+# list1 = [1,2,3]
+
+# list2 = list1.copy()
+# list2.reverse()
+
+# if(list1 == list2):
+#     print("YES")
+# else:
+#     print("NO")
+
+#-----------------------------------------------------
+
+# WAP to count the grade 'A' in given tuple
+
+tup = ('A','B','C','D','A')
+
+print(tup.count('A'))
+
+list = ['A','C','D','B','A']
+print(list)
+list.sort()
+print(list)
