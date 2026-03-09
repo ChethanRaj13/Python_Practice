344c869c983b437b892211902317f50131875a41
ChethanRaj13 <chethanrajjee@gmail.com>
2026-03-09 09:23:14 +0530

    condition statement

--- practice2.py
+++ practice2.py
@@ -1,48 +1,48 @@
 # WAP to input user's first name and print the length
 
-# name = input("Enter your first name: ")
-# print(name,len(name))
+name = input("Enter your first name: ")
+print(name,len(name))
 
 #-----------------------------------------------------
 
 # WAP to find the occurance of '$'
 
-# str = "aaaaa$a$a$a$"
-# print(str.find("$"))
+str = "aaaaa$a$a$a$"
+print(str.find("$"))
 
 #-----------------------------------------------------
 
 # WAP to check if the number is odd or even
 
-# num = int(input("Enter a number: "))
+num = int(input("Enter a number: "))
 
-# if(num % 2 == 0):
-#     print("The number is Even")
-# else: 
-#     print("The number is Odd")
+if(num % 2 == 0):
+    print("The number is Even")
+else: 
+    print("The number is Odd")
 
 #-----------------------------------------------------
 
 # WAP to find greatest of 3 numbers
 
-# num1 = int(input("Enter 1st number: "))
-# num2 = int(input("Enter 2nd number: "))
-# num3 = int(input("Enter 3rd number: "))
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+num3 = int(input("Enter 3rd number: "))
 
-# if(num1 > num3 and num1 > num2):
-#     print("the greatest number is",num1)
-# elif(num2 > num1 and num2 > num3):
-#     print("the greatest number is",num2)
-# else:
-#     print("the greatest number is",num3)
+if(num1 > num3 and num1 > num2):
+    print("the greatest number is",num1)
+elif(num2 > num1 and num2 > num3):
+    print("the greatest number is",num2)
+else:
+    print("the greatest number is",num3)
 
 #-----------------------------------------------------
 
 # WAP to check if the number is multiple of 7 
 
-# num = int(input("Enter the number: "))
+num = int(input("Enter the number: "))
 
-# if(num % 7 == 0):
-#     print("Yes")
-# else:
-#     print("No")
+if(num % 7 == 0):
+    print("Yes")
+else:
+    print("No")
