0ab5f3fc26f7e60a190435f1841ff3dc0848ce73
ChethanRaj13 <chethanrajjee@gmail.com>
2026-03-09 08:55:45 +0530

    strings

---
+++ practice2.py
@@ -0,0 +1,13 @@
+# WAP to input user's first name and print the length
+
+# name = input("Enter your first name: ")
+# print(name,len(name))
+
+#-----------------------------------------------------
+
+# WAP to find the occurance of '$'
+
+# str = "aaaaa$a$a$a$"
+# print(str.find("$"))
+
+#-----------------------------------------------------
