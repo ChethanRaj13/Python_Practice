3d6eb4a2f7749d865e4da1aeb3c133c95c519a55
ChethanRaj13 <chethanrajjee@gmail.com>
2026-03-09 09:22:07 +0530

    condition statement

--- practice2.py
+++ practice2.py
@@ -11,3 +11,38 @@
 # print(str.find("$"))
 
 #-----------------------------------------------------
+
+# WAP to check if the number is odd or even
+
+# num = int(input("Enter a number: "))
+
+# if(num % 2 == 0):
+#     print("The number is Even")
+# else: 
+#     print("The number is Odd")
+
+#-----------------------------------------------------
+
+# WAP to find greatest of 3 numbers
+
+# num1 = int(input("Enter 1st number: "))
+# num2 = int(input("Enter 2nd number: "))
+# num3 = int(input("Enter 3rd number: "))
+
+# if(num1 > num3 and num1 > num2):
+#     print("the greatest number is",num1)
+# elif(num2 > num1 and num2 > num3):
+#     print("the greatest number is",num2)
+# else:
+#     print("the greatest number is",num3)
+
+#-----------------------------------------------------
+
+# WAP to check if the number is multiple of 7 
+
+# num = int(input("Enter the number: "))
+
+# if(num % 7 == 0):
+#     print("Yes")
+# else:
+#     print("No")
