3abd115fdf2de834f02bef88892eae93104e0581
ChethanRaj13 <chethanrajjee@gmail.com>
2026-03-10 19:09:15 +0530

    while loop

---
+++ practice5.py
@@ -0,0 +1,55 @@
+# WAP to print 1 to 100
+
+# i = 1
+# while i <= 100:
+#     print(i)
+#     i += 1 
+
+#-------------------------------------------------------------
+
+#WAP to print 100 to 1
+
+# i = 100
+# while i > 0:
+#     print(i)
+#     i -= 1
+
+#-------------------------------------------------------------
+
+# WAP to print multiple of table
+
+# num = int(input("Enter the number: "))
+
+# i = 1
+
+# while i <= 10:
+#     print(num * i)
+#     i += 1
+
+#-------------------------------------------------------------
+
+# WAP to print all the elements of list 
+
+# list = [1,2,3,4,5,6,7,8,9,10]
+
+# i = 0
+# while i < len(list):
+#     print(list[i])
+#     i += 1
+
+#-------------------------------------------------------------
+
+# linear search
+
+# list = [1,2,3,4,5,6,7,8,9,10]
+
+# num = int(input("Enter the number to search: "))
+
+# i = 0
+# while i < len(list) and list[i] != num:
+#     i+=1
+
+# if i == len(list):
+#     print("not found")
+# else:
+#     print("found")    
